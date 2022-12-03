d8db10430ff86e64ccb9edc1f731ee2f60c76fe5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-12-03 11:56:22 +0530

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,10 +3,13 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { Product } from '../../components';
 import { client, urlFor } from '../../lib/client';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { incQty, decQty, qty, onAdd } = useStateContext();
+
   return (
     <div>
       <div className='product-detail-container'>
@@ -16,7 +19,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className='small-images-container'>
             {image?.map((item, i) => (
-              <img src={urlFor(item)} className={(i === index) ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} />
+              <img key={`${i}-${name}`} src={urlFor(item)} className={(i === index) ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} />
             ))}
           </div>
         </div>
@@ -38,13 +41,13 @@ const ProductDetails = ({ product, products }) => {
           <div className='quantity'>
             <h3>Quantity:</h3>
             <p className='quantity-desc'>
-              <span className='minus' onClick={() => {}}><AiOutlineMinus /></span>
-              <span className='num' onClick={() => {}}>0</span>
-              <span className='plus' onClick={() => {}}><AiOutlinePlus /></span>
+              <span className='minus' onClick={() => { decQty() }}><AiOutlineMinus /></span>
+              <span className='num' onClick={() => {}}>{ qty }</span>
+              <span className='plus' onClick={() => { incQty() }}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className='buttons'>
-            <button type='button' className='add-to-cart' onClick={() => {}}>Add to Cart</button>
+            <button type='button' className='add-to-cart' onClick={() => { onAdd(product, qty) }}>Add to Cart</button>
             <button type='button' className='buy-now' onClick={() => {}}>Buy Now</button>
           </div>
         </div>
