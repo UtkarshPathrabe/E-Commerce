eb69ecf30032422902687f0bf10e365f9c5d6316
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-12-03 22:58:41 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,6 +9,9 @@ const Layout = ({ children }) => {
     <div className='layout'>
       <Head>
         <title>Utkarsh Store</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Modern Full Stack E-Commerce Application created by Utkarsh Pathrabe using NextJS, ReactJS, Stripe and Sanity" />
+        <meta name="keywords" content="E-Commerce, Utkarsh Pathrabe, NextJS, ReactJS, Sanity, Stripe" />
       </Head>
       <header>
         <Navbar />
